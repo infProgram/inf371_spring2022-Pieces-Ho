4cd94b6e7a90893ac324e9ffd9eef2c9a984b0e9
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-16 17:11:26 +0800

    Test if 'Strength' 'Speed' in character dictionary

--- intro/4dictionaryCharacter.py
+++ intro/4dictionaryCharacter.py
@@ -1,10 +1,21 @@
 characterAlice = {'Strength':12, 'Dexterity':13, 'Constitution':9, 'Intelligence':2, 'Wisdom':6, 'Charisma':18}
 
-#print the entire dictionary
+# print the entire dictionary
 print(characterAlice)
 
-#print the dictionary by for-loop
+# print the dictionary by for-loop
 for key in characterAlice:
     print(key, end = "  ")
+print()
 
+# Test if 'Strength' in character dictionary
+if 'Strength' in characterAlice:
+    print("The Strength is ", characterAlice['Strength'])
+else:
+    print("Strength is not a character stat.")
 
+# Test if 'Speed' in character dictionary
+if 'Speed' in characterAlice:
+    print("The Speed is ", characterAlice['Speed'])
+else:
+    print("Speed is not a character stat.")
