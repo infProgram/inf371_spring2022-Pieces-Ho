bd406c4279d18c3878ac6d6cb44e159ab586f38d
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-16 18:08:58 +0800

    Add the quit in while

--- intro/6functionConvertTemperature.py
+++ intro/6functionConvertTemperature.py
@@ -2,9 +2,13 @@ def convertCelciusToFahrenheit(cel):
     print("convert!")
 
 if __name__ == "__main__":
-    
-    cel = input("Input the Celcius.")
-    convertCelciusToFahrenheit(cel)
+    isRunning = True
+    while isRunning:
+        cel = input("Input the Celcius.")
+        convertCelciusToFahrenheit(cel)
+        q = input("Quit now? (input Q)")
+        if q == 'q'or q =='Q':
+            isRunning = False
     
 
 
