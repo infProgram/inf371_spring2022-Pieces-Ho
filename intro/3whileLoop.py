57590ee46b89e4e82d20c6562cb9064a4abc5586
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-16 16:18:52 +0800

    Enter 'q' to quit

---
+++ intro/3whileLoop.py
@@ -0,0 +1,14 @@
+from operator import truediv
+from os import ftruncate
+
+
+
+isRunning = True
+
+while isRunning:
+    str = input("Quit? (input 'Q')")
+    if str == 'q':
+        isRunning = False
+    
+
+
