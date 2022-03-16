e82b309b3accae992ab4fdafc4221f11f0edcb82
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-16 18:05:09 +0800

    Test the funtion

--- intro/6functionConvertTemperature.py
+++ intro/6functionConvertTemperature.py
@@ -1,6 +1,12 @@
 def convertCelciusToFahrenheit(cel):
     print("convert!")
 
+if __name__ == "__main__":
+    
+    cel = input("Input the Celcius.")
+    convertCelciusToFahrenheit(cel)
+    
+
 
 
     
