b768d1325c9fc1ab494205697a09dfab904ca80b
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-16 17:33:58 +0800

    Create an empty dictionary

--- intro/5dictionaryRandomCharacter.py
+++ intro/5dictionaryRandomCharacter.py
@@ -3,3 +3,8 @@ import random
 # create a list of character keys
 keys = ["strength", "dexterity", "constitution", "intelligence", "wisdom", "charisma"]
 
+# create an empty dictionary
+dictionaryBeth = {}
+
+
+
