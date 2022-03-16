68648d300cb24744ac0731b85f3f8fff9a901a4e
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-16 15:07:52 +0800

    a cute zooList for task1

--- intro/1forList.py
+++ intro/1forList.py
@@ -1,2 +1,19 @@
 
 print("test the github and VSC！")
+
+# in a zoo
+zoolist = ["dog", "cat", "bird", "horse", "tiger", "snake", "monkey", "sheep", "mouse", "rabbit", "deer"]
+
+# the first way to print list
+print(zoolist)
+
+# the second 
+print("There are some ")
+for animals in zoolist:
+    print(animals, end =" ")
+
+# the third 
+print()
+print("There are", len(zoolist), "kinds of animals.")
+for i in range(len(zoolist)):
+    print("This is a " + zoolist[i])
