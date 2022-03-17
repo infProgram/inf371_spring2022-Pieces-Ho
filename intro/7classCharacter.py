93ad63341b96883cc51fa479fed75badcc9aa623
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-17 12:36:33 +0800

    class Character

---
+++ intro/7classCharacter.py
@@ -0,0 +1,20 @@
+
+
+class Character(object):
+    def __init__(self, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma) :
+        self.strength = Strength
+        self.dexterity = Dexterity
+        self.constitution = Constitution
+        self.intelligence = Intelligence
+        self.wisdom = Wisdom
+        self.charisma = Charisma
+        self.hitpoint = Constitution * 30 + 50
+
+   
+
+
+    
+
+
+
+
