6207c82907efd2ff65c2aecae678ac731c833f2a
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-17 12:40:20 +0800

    create the methods and main

--- intro/7classCharacter.py
+++ intro/7classCharacter.py
@@ -1,4 +1,4 @@
-
+import random
 
 class Character(object):
     def __init__(self, Strength, Dexterity, Constitution, Intelligence, Wisdom, Charisma) :
@@ -10,7 +10,19 @@ class Character(object):
         self.charisma = Charisma
         self.hitpoint = Constitution * 30 + 50
 
-   
+    def PrintPlayerStats(self):
+        pass      
+    def PrintHitpoints(self):
+        pass
+    def Attack(self):
+        pass
+    def Defense(self):
+        pass
+    def Heal(self):
+        pass
+
+if __name__ == "__main__":
+    pass
 
 
     
