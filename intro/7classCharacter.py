88ff0a5f6568923f6b0e33bfd5f12ad004ad1e39
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-03-18 00:37:55 +0800

    Alice and Ben fight the Monster

--- intro/7classCharacter.py
+++ intro/7classCharacter.py
@@ -11,15 +11,16 @@ class Character(object):
         self.hitpoint = Constitution * 30 + 50
 
     def PrintPlayerStats(self):
-        print("Strength", self.strength)
-        print("Dexterity", self.dexterity)
-        print("Constitution", self.constitution)
-        print("Intelligence", self.intelligence)
-        print("Wisdom", self.wisdom)
-        print("Charisma", self.charisma)
+        print("Strength:", self.strength, end = "\t")
+        print("Dexterity:", self.dexterity, end = "\t")
+        print("Constitution:", self.constitution, end = "\t")
+        print("Intelligence:", self.intelligence, end = "\t")
+        print("Wisdom:", self.wisdom, end = "\t")
+        print("Charisma:", self.charisma, end = "\t")
+        print("Hitpoint:", self.hitpoint)
         
     def PrintHitpoints(self):
-        print("Hitpoint", self.hitpoint)
+        print("Hitpoint:", self.hitpoint)
 
     def Attack(self):
         return random.randint(1, self.strength)
@@ -33,7 +34,31 @@ class Character(object):
         self.hitpoint += value
 
 if __name__ == "__main__":
-    pass
+    Alice = Character(7, 18, 10, 13, 14, 17)
+    Ben = Character(18, 11, 17, 10, 9, 12)
+    Monster = Character(15, 5, 10, 3, 3, 3)
+    print("Alice stats:")
+    Alice.PrintPlayerStats()
+    print("Ben stats:")
+    Ben.PrintPlayerStats()
+    print("Monster stats:")
+    Monster.PrintPlayerStats()
+
+    print("---FIGHT---")
+    Alice.Defense(Monster.Attack())
+    Ben.Defense(Monster.Attack())
+    print("---FIGHT OVER---")
+    print("Alice", end = " ")
+    Alice.PrintHitpoints()
+    print("Ben", end = " ")
+    Ben.PrintHitpoints()
+    print("---HEAL BEN---")
+    Ben.Heal(5)
+    print("Alice", end = " ")
+    Alice.PrintHitpoints()
+    print("Ben", end = " ")
+    Ben.PrintHitpoints()
+
 
 
 
