ae8edc562eec7948a9279eeea5c8af7529a8c24d
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-05-26 15:52:40 +0800

    Add draw in Oneball Class

--- pygameTutorials/ballExtend.py
+++ pygameTutorials/ballExtend.py
@@ -7,8 +7,11 @@ class OneBall:
      self.ballSpeedY = speedY
      self.x = x
      self.y = y
-     self.ball = pygame.draw.circle(screen, colour, [x, y], 40, width=0)
-     self.ballFrame = pygame.draw.circle(screen, (0,0,0), [x, y], 40, width=5)
+     self.ballColour = colour
+     
+  def draw(self):
+    self.ball = pygame.draw.circle(screen, self.ballColour, [self.x, self.y], 40, width=0)
+    self.ballFrame = pygame.draw.circle(screen, (0,0,0), [self.x, self.y], 40, width=5)
 
   def move(self):
       if self.ball.left <= 0 or self.ball.right >= width:     #如果球碰到左或者右边缘，就将球横向速度取反
@@ -16,12 +19,12 @@ class OneBall:
       if self.ball.top <= 0 or self.ball.bottom >= height:    #如果球碰到左或者右边缘，就将球纵向速度取反
         self.ballSpeedY = - self.ballSpeedY
 
-      x = x + self.ballSpeedX
-      y = y + self.ballSpeedY
+      self.x = self.x + self.ballSpeedX
+      self.y = self.y + self.ballSpeedY
 
 
 
-#ballSpeed = [2, 2]
+# ballSpeed = [2, 2]
 size = width, height = 640, 480
 backgroundColor = (104,183,150)
 screen = pygame.display.set_mode(size)
@@ -29,7 +32,7 @@ pygame.display.set_caption('Ball Game') #主窗口标题
 
 # x = 320
 # y = 240
-
+ball001 = OneBall(2,2,320,240,(162,56,31))
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
@@ -37,18 +40,20 @@ while isRunning:
   
   screen.fill(backgroundColor)
 
-  ball001 = OneBall
+  ball001.draw()
+  ball001.move()
 
   # ball = pygame.draw.circle(screen, (162,56,31), [x, y], 40, width=0)
   # frame = pygame.draw.circle(screen, (0,0,0), [x, y], 40, width=5)
+  # ballRect = ball.
   
   # if ball.left <= 0 or ball.right >= width:     #如果球碰到左或者右边缘，就将球横向速度取反
   #       ballSpeed[0] = - ballSpeed[0]
   # if ball.top <= 0 or ball.bottom >= height:    #如果球碰到左或者右边缘，就将球纵向速度取反
   #       ballSpeed[1] = - ballSpeed[1]
 
-  # x = x+ballSpeed[0]
-  # y = y+ballSpeed[1]
+  # x = x + ballSpeed[0]
+  # y = y + ballSpeed[1]
 
 
 
