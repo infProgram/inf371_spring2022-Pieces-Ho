1359a47fdb8eac50c71ce87d9072991e3a876f18
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-05-26 16:03:11 +0800

    Two Balls!

--- pygameTutorials/ballExtend.py
+++ pygameTutorials/ballExtend.py
@@ -14,25 +14,21 @@ class OneBall:
     self.ballFrame = pygame.draw.circle(screen, (0,0,0), [self.x, self.y], 40, width=5)
 
   def move(self):
-      if self.ball.left <= 0 or self.ball.right >= width:     #如果球碰到左或者右边缘，就将球横向速度取反
+      if self.ball.left <= 0 or self.ball.right >= width:     
         self.ballSpeedX = - self.ballSpeedX
-      if self.ball.top <= 0 or self.ball.bottom >= height:    #如果球碰到左或者右边缘，就将球纵向速度取反
+      if self.ball.top <= 0 or self.ball.bottom >= height:   
         self.ballSpeedY = - self.ballSpeedY
 
       self.x = self.x + self.ballSpeedX
       self.y = self.y + self.ballSpeedY
 
-
-
-# ballSpeed = [2, 2]
 size = width, height = 640, 480
 backgroundColor = (104,183,150)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Ball Game') #主窗口标题
 
-# x = 320
-# y = 240
 ball001 = OneBall(2,2,320,240,(162,56,31))
+ball002 = OneBall(3,4,200,100,(255,255,255))
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
@@ -42,19 +38,8 @@ while isRunning:
 
   ball001.draw()
   ball001.move()
-
-  # ball = pygame.draw.circle(screen, (162,56,31), [x, y], 40, width=0)
-  # frame = pygame.draw.circle(screen, (0,0,0), [x, y], 40, width=5)
-  # ballRect = ball.
-  
-  # if ball.left <= 0 or ball.right >= width:     #如果球碰到左或者右边缘，就将球横向速度取反
-  #       ballSpeed[0] = - ballSpeed[0]
-  # if ball.top <= 0 or ball.bottom >= height:    #如果球碰到左或者右边缘，就将球纵向速度取反
-  #       ballSpeed[1] = - ballSpeed[1]
-
-  # x = x + ballSpeed[0]
-  # y = y + ballSpeed[1]
-
+  ball002.draw()
+  ball002.move()
 
 
   time.sleep(0.009)
