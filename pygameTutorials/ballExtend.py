82199b1501fde9b8b6be3e859f1dd8c250ec5acd
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-05-26 20:23:44 +0800

    The first way I think to collide(but not perfect)

--- pygameTutorials/ballExtend.py
+++ pygameTutorials/ballExtend.py
@@ -8,6 +8,8 @@ class OneBall:
      self.x = x
      self.y = y
      self.ballColour = colour
+     self.ball = pygame.draw.circle(screen, self.ballColour, [self.x, self.y], 40, width=0)
+     self.ballFrame = pygame.draw.circle(screen, (0,0,0), [self.x, self.y], 40, width=5)
      
   def draw(self):
     self.ball = pygame.draw.circle(screen, self.ballColour, [self.x, self.y], 40, width=0)
@@ -21,16 +23,28 @@ class OneBall:
 
       self.x = self.x + self.ballSpeedX
       self.y = self.y + self.ballSpeedY
-  def collide(self, OneBall):
-    flag = pygame.sprite.collide_circle(self.ball,OneBall.ball)
-    if flag:
-      print("Boom!")
+  def getBall(self):
+      
+      pass
 
-    # if self.ball.colliderect(OneBall.ball):
-    #   self.ballSpeedX = - self.ballSpeedX
-      # self.ballSpeedY = - self.ballSpeedY
-    self.x = self.x + self.ballSpeedX
-    self.y = self.y + self.ballSpeedY
+  def collide(self, OneBall):
+      print("In Method.",end=" ")
+      
+      if self.ball.colliderect(OneBall.ball):
+        print("Collide.",end="\n\n")
+
+        self.ballSpeedX = - self.ballSpeedX
+        self.ballSpeedY = - self.ballSpeedY
+        self.x = self.x + self.ballSpeedX * 0.5
+        self.y = self.y + self.ballSpeedY * 0.5
+
+        # if self.ball.left <= OneBall.ball.right or self.ball.right >= OneBall.ball.left:     
+        #   self.ballSpeedX = - self.ballSpeedX
+        #   self.x = self.x + self.ballSpeedX * 0.5
+        # if self.ball.top <= OneBall.ball.bottom or self.ball.bottom >= OneBall.ball.top:   
+        #   self.ballSpeedY = - self.ballSpeedY
+        #   self.y = self.y + self.ballSpeedY * 0.5
+
     
 
 size = width, height = 640, 480
@@ -39,22 +53,21 @@ screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Ball Game') #主窗口标题
 
 ball001 = OneBall(2,2,320,240,(162,56,31))
-ball002 = OneBall(3,3,400,100,(255,255,255))
+ball002 = OneBall(3,4,200,100,(255,255,255))
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
     if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT: sys.exit()
-  
   screen.fill(backgroundColor)
 
   ball001.draw()
-  ball001.move()
   ball002.draw()
+  ball001.move()
   ball002.move()
 
   ball002.collide(ball001)
-
+ 
+  
 
   time.sleep(0.009)
-  
   pygame.display.flip()  # 刷新整个界面显示  # pygame.display.update(_) 刷新指定部分显示
