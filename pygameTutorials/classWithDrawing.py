4e74fd21b5176ff62cca90d4a4d2d00f5f68dd6d
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-05-27 08:01:09 +0800

    There are four balls here.

--- pygameTutorials/classWithDrawing.py
+++ pygameTutorials/classWithDrawing.py
@@ -46,28 +46,37 @@ pygame.display.set_caption('Ball Game') #  主窗口标题（Title）
 ball001 = OneBall(2,2,320,240,(162,56,31))
 ball002 = OneBall(3,4,200,100,(255,255,255))
 ball003 = OneBall(-2,-3,450,330,(0,255,0))
+ball004 = OneBall(6,2,90,110,(0,0,255))
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
     if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT: sys.exit()
   screen.fill(backgroundColor)
 
-  ball001.draw()
+  ball001.draw()  # How many balls, how many draws and move.
   ball002.draw()
   ball003.draw()
+  ball004.draw()
 
   ball001.move()
   ball002.move()
   ball003.move()
+  ball004.move()
 
-  ball002.collide(ball001)
+  ball002.collide(ball001)  # I realize that more balls more collide for two of them.
   ball001.collide(ball002)
 
   ball003.collide(ball001)
-  ball001.collide(ball003)
-
   ball003.collide(ball002)
+  ball001.collide(ball003)
   ball002.collide(ball003)
+
+  ball004.collide(ball001)  # Add the fourth ball, more collide
+  ball004.collide(ball002)
+  ball004.collide(ball003)
+  ball001.collide(ball004)
+  ball002.collide(ball004)
+  ball003.collide(ball004)
 
   time.sleep(0.009)
   pygame.display.flip()  # 刷新整个界面显示  # pygame.display.update(_) 刷新指定部分显示
