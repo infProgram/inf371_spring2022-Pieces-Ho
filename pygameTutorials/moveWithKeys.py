b6f4b0cf6a344ae2ed1e1b121381be1f75276772
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-05-28 11:23:22 +0800

    one ball move

--- pygameTutorials/moveWithKeys.py
+++ pygameTutorials/moveWithKeys.py
@@ -2,49 +2,14 @@ import sys, pygame, time
 pygame.init()
 # Try to modify this class to be with Image
 
-class OneBall:
-  def __init__(self,filename,speedX,speedY,x,y) -> None:
-     self.picture = pygame.image.load(filename)
-     self.rect = self.picture.get_rect()
-     self.ballSpeedX = speedX
-     self.ballSpeedY = speedY
-     self.rect.x = x
-     self.rect.y = y
-
-  def move(self):
-      if self.rect.left <= 0 or self.rect.right >= width:     
-        self.ballSpeedX = - self.ballSpeedX
-        self.rect.x = self.rect.x + self.ballSpeedX * 9
-      if self.rect.top <= 0 or self.rect.bottom >= height:   
-        self.ballSpeedY = - self.ballSpeedY
-        self.rect.y = self.rect.y + self.ballSpeedY * 9
-
-      self.rect.x = self.rect.x + self.ballSpeedX
-      self.rect.y = self.rect.y + self.ballSpeedY
-
-  def collide(self, OneBall):   
-      if self.rect.colliderect(OneBall.rect):
-        print("Collide.")
-        self.ballSpeedX = - self.ballSpeedX
-        self.ballSpeedY = - self.ballSpeedY
-        self.rect.x = self.rect.x + self.ballSpeedX * 10  #when collide,they back in 10 times speed
-        self.rect.y = self.rect.y + self.ballSpeedY * 10
-
-  def blit(self, screen):
-      screen.blit(self.picture, self.rect)
-      
-
 size = width, height = 1040, 780
 backgroundColor = (104,183,150)
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Ball Game') #  主窗口标题（Title）
 
-ball001 = OneBall("soccer.png",4,3,520,340)
-ball002 = OneBall("basketball.png",3,-4,700,100)
-
 # key ball
 site = x,y = 200,100
-speed = speedxX,speedY = 3,-4
+speedX = speedY = 3
 ball_key = pygame.image.load("basketball.png")
 ball_rect =ball_key.get_rect()
 ball_rect.x = x
@@ -56,25 +21,13 @@ while isRunning:
     if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT: sys.exit()
     # control key ball
   keyPressed =  pygame.key.get_pressed()
-  if keyPressed[pygame.K_w]: ball_rect.y = ball_rect.y - 3
-  if keyPressed[pygame.K_s]: ball_rect.y = ball_rect.y + 3
-  if keyPressed[pygame.K_a]: ball_rect.x = ball_rect.x - 3
-  if keyPressed[pygame.K_d]: ball_rect.x = ball_rect.x + 3
+  if keyPressed[pygame.K_w]: ball_rect.y = ball_rect.y - speedY
+  if keyPressed[pygame.K_s]: ball_rect.y = ball_rect.y + speedY
+  if keyPressed[pygame.K_a]: ball_rect.x = ball_rect.x - speedX
+  if keyPressed[pygame.K_d]: ball_rect.x = ball_rect.x + speedX
   screen.fill(backgroundColor)
-
-  
-  ball001.blit(screen)
-  ball001.move()
 
   screen.blit(ball_key,ball_rect)
 
-
-#   ball002.blit(screen)
-#   ball002.move()
-
-#   ball001.collide(ball002)
-#   ball002.collide(ball001)
-
-
-  time.sleep(0.003)
+  time.sleep(0.002)
   pygame.display.flip()  # 刷新整个界面显示  # pygame.display.update(_) 刷新指定部分显示
