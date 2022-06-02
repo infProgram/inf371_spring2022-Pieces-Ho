d5dbfa7f7741d98795462d13fb674d202566a039
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-06-02 08:49:49 +0800

    Add background pic and check it

--- peronalProject/bubbleDefense.py
+++ peronalProject/bubbleDefense.py
@@ -5,13 +5,14 @@ size = width, height = 460, 680
 backgroundColor = (211,211,211) # green background looks like basketball court
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Bubble Defense') #  主窗口标题（Title）
-
+backgroundPic = pygame.image.load(r"C:\Users\MZ Huo\Desktop\628.png")
 
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
     if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT: sys.exit()
-  screen.fill(backgroundColor)
+  screen.blit(backgroundPic,(0,0))
+#   screen.fill(backgroundColor)
 
 
   time.sleep(0.007)
