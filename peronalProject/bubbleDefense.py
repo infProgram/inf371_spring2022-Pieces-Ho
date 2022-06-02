ce563f2bad84b29916e0448a90c6cac6c35a6d1f
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-06-02 10:33:44 +0800

    Draw the laucher

--- peronalProject/bubbleDefense.py
+++ peronalProject/bubbleDefense.py
@@ -1,5 +1,24 @@
 import sys, pygame, time
 pygame.init()
+# launcher class
+class launcher():
+    def __init__(self) -> None:
+        
+        self.speedx = self.speedy = 20
+        self.x = 200
+        self.y = 660
+        self.points = [(self.x,self.y),(self.x+20,self.y),(self.x+20,self.y-20),(self.x+40,self.y-20),(self.x+40,self.y),(self.x+60,self.y),(self.x+60,self.y+20),(self.x,self.y+20)]
+        self.tank = pygame.draw.polygon(screen, (102,153,102),self.points)
+        # self.tankFrame = pygame.draw.circle(screen, (0,0,0), self.points, width=5)
+
+    def draw(self):
+        pass
+    def move(self):
+        pass
+
+
+
+
 
 size = width, height = 460, 680
 screen = pygame.display.set_mode(size)
@@ -14,6 +33,7 @@ while isRunning:
   
   for i in range(20,441,20): pygame.draw.line(screen,(112,128,144),[i,0],[i,680],1) # Draw xy lines
   for i in range(20,681,20): pygame.draw.line(screen,(112,128,144),[0,i],[460,i],1)
+  lanch = launcher()
 
   time.sleep(0.007)
   pygame.display.flip()  # 刷新整个界面显示
