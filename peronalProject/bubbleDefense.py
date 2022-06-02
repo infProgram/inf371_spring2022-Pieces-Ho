438e4e488a8f9fae2deb2b1f0b42403b3cabae16
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-06-02 11:02:38 +0800

    It can move by mouse and not out window

--- peronalProject/bubbleDefense.py
+++ peronalProject/bubbleDefense.py
@@ -2,19 +2,27 @@ import sys, pygame, time
 pygame.init()
 # launcher class
 class launcher():
-    def __init__(self) -> None:
-        
-        self.speedx = self.speedy = 20
+    def __init__(self) -> None:    
+        self.speed = 20
         self.x = 200
         self.y = 660
         self.points = [(self.x,self.y),(self.x+20,self.y),(self.x+20,self.y-20),(self.x+40,self.y-20),(self.x+40,self.y),(self.x+60,self.y),(self.x+60,self.y+20),(self.x,self.y+20)]
         self.tank = pygame.draw.polygon(screen, (102,153,102),self.points)
-        # self.tankFrame = pygame.draw.circle(screen, (0,0,0), self.points, width=5)
+        self.tankFrame = pygame.draw.polygon(screen, (46,139,87), self.points, width=2)
 
     def draw(self):
-        pass
-    def move(self):
-        pass
+        self.tank = pygame.draw.polygon(screen, (102,153,102),self.points)
+        self.tankFrame = pygame.draw.polygon(screen, (46,139,87), self.points, width=2)
+    def move(self,key):
+        if key == 'W': pass
+        if key == 'A': self.x = self.x - self.speed
+        if key == 'D': self.x = self.x + self.speed
+        print("x: ",self.x, ",y: ",self.y)
+
+        if self.x <= 0 :  self.x = 0
+        if self.x >= width-60: self.x = width-60
+        self.points = [(self.x,self.y),(self.x+20,self.y),(self.x+20,self.y-20),(self.x+40,self.y-20),(self.x+40,self.y),(self.x+60,self.y),(self.x+60,self.y+20),(self.x,self.y+20)]
+        
 
 
 
@@ -24,16 +32,22 @@ size = width, height = 460, 680
 screen = pygame.display.set_mode(size)
 pygame.display.set_caption('Bubble Defense') #  主窗口标题（Title）
 backgroundPic = pygame.image.load("bgPic.png")
+mylancher = launcher()
 
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
     if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT: sys.exit()
+    if event.type == pygame.KEYDOWN:
+        keyPressed =  pygame.key.get_pressed()
+        if keyPressed[pygame.K_w]: mylancher.move('W')
+        if keyPressed[pygame.K_a]: mylancher.move('A')
+        if keyPressed[pygame.K_d]: mylancher.move('D')
   screen.blit(backgroundPic,(0,0))
-  
   for i in range(20,441,20): pygame.draw.line(screen,(112,128,144),[i,0],[i,680],1) # Draw xy lines
   for i in range(20,681,20): pygame.draw.line(screen,(112,128,144),[0,i],[460,i],1)
-  lanch = launcher()
 
-  time.sleep(0.007)
+  mylancher.draw()
+
+  time.sleep(0.01)
   pygame.display.flip()  # 刷新整个界面显示
