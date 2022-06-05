3b64b05708ea14cbc0884f34e4593f79a6a03f8a
Pieces-Ho <1248541901+Pieces-Ho@users.noreply.github.com>
2022-06-05 22:36:23 +0800

    Try to make the bullet but not work now

--- peronalProject/bubbleDefense.py
+++ peronalProject/bubbleDefense.py
@@ -24,21 +24,15 @@ class launcher():
 
 
     def move(self,key):
-        if key == 'W':  
-            mybullet = bullet(self.x,self.y)
-            mybullet.drawBullet()
-            print("W be pressed!")
-            
         if key == 'A': self.x = self.x - self.speed
         if key == 'D': self.x = self.x + self.speed
       
         if self.x <= 0 :  self.x = 0
         if self.x >= width-60: self.x = width-60
         self.points = [(self.x,self.y),(self.x+20,self.y),(self.x+20,self.y-20),(self.x+40,self.y-20),(self.x+40,self.y),(self.x+60,self.y),(self.x+60,self.y+20),(self.x,self.y+20)]
-        
-    def bulletFly(self):
-        self.bullety = self.bullety - self.bulletSpeed
-        print("x: ",self.bulletx, ",y: ",self.bullety)
+    # def bulletFly(self):
+    #     self.bullety = self.bullety - self.bulletSpeed
+    #     print("x: ",self.bulletx, ",y: ",self.bullety)
 
 
 class bullet():
@@ -51,6 +45,7 @@ class bullet():
         self.bulletFrame = pygame.draw.rect(screen,(139,121,94),[self.bulletx,self.bullety,20,20],1)
 
     def drawBullet(self):
+        print("Press W!")
         self.bullet = pygame.draw.rect(screen,self.bulletColour,[self.bulletx,self.bullety,20,20],0)
         self.bulletFrame = pygame.draw.rect(screen,(139,121,94),[self.bulletx,self.bullety,20,20],1)
 
@@ -88,38 +83,35 @@ pygame.display.set_caption('Bubble Defense') #  主窗口标题（Title）
 backgroundPic = pygame.image.load("bgPic.png")
 mylancher = launcher()
 mywall = wall()
-smallWhile = 0
+mybullet = bullet(mylancher.x,mylancher.y)
+wallWhile = 0
 
 isRunning = True
 while isRunning:
   for event in pygame.event.get():
     if (event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE) or event.type == pygame.QUIT: sys.exit()
-    if event.type == pygame.KEYDOWN:
+    if event.type == pygame.KEYDOWN: 
         keyPressed =  pygame.key.get_pressed()
         if keyPressed[pygame.K_w]: 
-            mybullet = bullet(mylancher.x,mylancher.y)
             mybullet.drawBullet()
-            # mylancher.move('W')
         if keyPressed[pygame.K_a]: mylancher.move('A')
         if keyPressed[pygame.K_d]: mylancher.move('D')
   screen.blit(backgroundPic,(0,0))
 
-  if smallWhile == 50:  # make a smaller while
+  if wallWhile == 50:  # wall move speed in smaller while
       mywall.move()
-      pygame.draw.circle(screen, (0,0,0), [250, 200], 40, width=0)
-      smallWhile = 0
-  smallWhile = smallWhile+1
-
-
-  mywall.draw()
+      wallWhile = 0
+  wallWhile = wallWhile+1
+  mywall.draw() # Draw wall with xy lines
 
   for i in range(20,441,20): pygame.draw.line(screen,(112,128,144),[i,0],[i,680],1) # Draw xy lines
   for i in range(20,681,20): pygame.draw.line(screen,(112,128,144),[0,i],[460,i],1)
 
-  mylancher.draw()  # draw lancher without xy lines
+  mylancher.draw()  # Draw lancher without xy lines
+
   #   if  mylancher.bullety >0: 
   #      mylancher.bulletFly()
 
 
-  time.sleep(0.01)
+  time.sleep(0.5)
   pygame.display.flip()  # 刷新整个界面显示
